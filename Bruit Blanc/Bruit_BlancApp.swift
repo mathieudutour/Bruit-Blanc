201a01490f68eedc90408ed4af238228503f7686
mathieudutour <mathieu@dutour.me>
2022-04-08 15:05:20 +0200

    Initial Commit

---
+++ Bruit Blanc/Bruit_BlancApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bruit_BlancApp.swift
+//  Bruit Blanc
+//
+//  Created by Mathieu Dutour on 08/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct Bruit_BlancApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
